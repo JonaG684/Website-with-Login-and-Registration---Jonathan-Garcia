4d747ef63119032f964c25adc8f8ebd1ee75d997
Jonathan Garcia <122134746+JonaG684@users.noreply.github.com>
2023-07-23 22:20:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Website-with-Login-and-Registration---Jonathan-Garcia
+ A website with CSS, JavaScript and HTML with login and Registration.
---
+++ script.js
@@ -0,0 +1,31 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {wrapper.classList.add('active');});
+
+loginLink.addEventListener('click', () => {wrapper.classList.remove('active');});
+
+btnPopup.addEventListener('click', () => {wrapper.classList.add('active-popup');});
+
+iconClose.addEventListener('click', () => {wrapper.classList.remove('active-popup');});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close');
+  
+    if (wrapper && loginLink && registerLink && btnPopup && iconClose) {
+      // Resto del código con los event listeners
+    } else {
+      console.error("Alguno de los elementos no se encontró en el DOM.");
+    }
+  });
+
+
+
+
